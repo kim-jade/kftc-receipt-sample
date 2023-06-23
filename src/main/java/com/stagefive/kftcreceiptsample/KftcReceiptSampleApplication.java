c1bbae4e19bf72e2ef75509773227dbb15508ec9
jade <jade.kim@stagefive.com>
2023-06-23 18:25:04 +0900

    init: 프로젝트 구성

---
+++ src/main/java/com/stagefive/kftcreceiptsample/KftcReceiptSampleApplication.java
@@ -0,0 +1,13 @@
+package com.stagefive.kftcreceiptsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KftcReceiptSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KftcReceiptSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/stagefive/kftcreceiptsample/KftcReceiptSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stagefive.kftcreceiptsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KftcReceiptSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
